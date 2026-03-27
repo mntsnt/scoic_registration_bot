f80fac6479676a893d3188eed7f173d19332b1de
mntsnt <mintesnoh@gmail.com>
2026-03-27 22:30:23 +0300

    data

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 ADMIN_ID = os.environ.get("ADMIN_ID")  # Replace with your actual admin Telegram ID
 GROUP_LINK = os.environ.get("GROUP_LINK")  # Replace with your actual group invite link
+DB_NAME = "registrations.db"
